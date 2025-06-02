b94fec26290d988dd752bf25fea61f453a3d2b21
EclipseR33 <867226720@qq.com>
2025-06-02 22:40:22 +0800

    update settings

--- demos/chat_demos/collector.py
+++ demos/chat_demos/collector.py
@@ -37,7 +37,8 @@ def collecting_all_info(user_name, user_info):
         name = db.Column(db.String(80), unique=True, nullable=False)
         info = db.Column(db.String(200), unique=False, nullable=False)
 
-    DATABASE_URI = 'sqlite://///Users/meijiawei/Documents/metabci/MetaBCI/demos/chat_demos/users.db'
+    # DATABASE_URI = 'sqlite://///Users/meijiawei/Documents/metabci/MetaBCI/demos/chat_demos/users.db'
+    DATABASE_URI = './users.db'
 
     # 创建数据库引擎
     engine = create_engine(DATABASE_URI)
